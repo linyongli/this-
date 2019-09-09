3f6e4215c8c61c85bea778d8fd5ad90ba37037b5
linyongli <403202337@qq.com>
2019-09-09 15:43:22 +0800

    Add files via upload
    作用域与闭包的使用

---
+++ index.js
@@ -0,0 +1,102 @@
+//封装过的代码
+/*
+ author： 准备资源
+*/
+(function() {
+
+	//prepare
+	function prepare() {
+
+		const imgTask = (img, src) =>  {
+			return new Promise(function(resolve,reject)  {
+				img.onload =  resolve;
+				img.onerror = reject;
+				img.src = src;
+			})
+		}
+		const context = document.getElementById('content').getContext('2d');
+		const heroImg = new Image();
+		const allSpriteImg = new Image();
+
+		const allresourceTask = Promise.all([
+			imgTask(heroImg, './hero.png'),
+			imgTask(allSpriteImg, './all.jpg')
+		])
+
+		return {
+			/*
+			 * @param {Function} [callback]  -当准备好了之后要调用的回调函数
+			 */
+			getResource(callback) {
+				allresourceTask.then(function() {
+					callback && callback(context, heroImg, allSpriteImg)
+				})
+		
+			}
+		}
+	}
+
+	//draw
+	function drawHero(context, heroImg,allSpriteImg){
+		var draw = function() {
+			this.context
+			.drawImage(
+				this.img,
+				this.imgPos.x,
+				this.imgPos.y,
+				this.imgPos.width,
+				this.imgPos.height,
+				this.rect.x + 50,
+				this.rect.y,
+				this.rect.width,
+				this.rect.height
+			)
+		}
+		var hreo = {
+			img: heroImg,
+			context: context,
+			imgPos: {
+				x: 0,
+				y: 0,
+				width: 32,
+				height: 32
+			},
+
+			rect:{
+				x: 0,
+				y: 0,
+				width: 45,
+				height: 45,
+			},
+			draw:draw
+		}
+
+		var monster = {
+			img: allSpriteImg,
+			context: context,
+			imgPos: {
+				x: 859,
+				y: 529,
+				width: 32,
+				height: 32
+			},
+
+			rect:{
+				x: 100,
+				y: 100,
+				width: 45,
+				height: 45,
+			},
+			draw:draw
+		}
+
+		hreo.draw();
+		monster.draw();
+	}
+
+	var resourceManager = prepare();
+	resourceManager.getResource(function(context, heroImg,allSpriteImg) {
+		drawHero(context, heroImg,allSpriteImg)
+	});
+
+})()
